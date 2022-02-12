ed2222a3685841e9895149d2d67c7ef1731135e6
aky201086 <aky201086.amit@gmail.com>
2022-02-12 22:15:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ def processRequest(req):
         prediction = predict(description)
           
         fulfillmentText= "Predicted Accident Level  {} !".format(prediction)
+	print("###fulfillmentText:###### " fulfillmentText)
         #log.write_log(sessionID, "Bot Says: "+fulfillmentText)
         return {
             "fulfillmentText": fulfillmentText
